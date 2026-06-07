5f3aa2933d5a02af617b6374dad948c7d4d48da9
robindidier-dev <contact.robin.didier@gmail.com>
2026-06-07 15:41:36 +0200

    test avec cas de bord

--- source.c
+++ source.c
@@ -1,9 +1,43 @@
-main() {
-    d = {7:70, 8:80};
-    print(d[7]);
-    print(d[8]);
-    d[7] = 71;
-    print(d[7]);
-    print(d[8]);
-    return 0;
+function return_42() {
+    return 42;
+}
+
+function un_argument(a) {
+    return a * 2;
+}
+
+function max_arguments(a, b, c, d, e, f) {
+    return a + b + c + d + e + f;
+}
+
+function imbrication_calculs(a, b) {
+    return a + b;
+}
+
+function fibo(n) {
+    r = n;
+    if (n > 1) {
+        r = fibo(n - 1) + fibo(n - 2);
+    }
+    return r;
+}
+
+main(arg1, arg2) {
+        print(return_42());
+
+    print(un_argument(5));
+
+    print(max_arguments(1, 2, 3, 4, 5, 6));
+
+    print(imbrication_calculs(un_argument(10), un_argument(20)));
+
+    print(fibo(6));
+
+    compteur = 0;
+    while (compteur != 3) {
+        compteur = compteur + 1;
+    }
+    print(compteur);
+
+    return arg1 + arg2;
 }
